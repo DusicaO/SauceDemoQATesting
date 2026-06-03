ad3d0094f328c58e905beb1de210b7ae08c2a3be
DusicaO <dusicaobrovacki@gmail.com>
2026-06-03 02:08:11 +0200

    Added ChromeOptions for stable test execution

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,14 +4,18 @@ import application.URLs;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import pages.CartPage;
+import pages.Header;
 import pages.ProductsPage;
 import pages.StartingPage;
 
 import java.time.Duration;
+import java.util.Map;
 
 public class BaseTest {
 
@@ -21,6 +25,8 @@ public class BaseTest {
 
     public static StartingPage startingPage;
     public static ProductsPage productsPage;
+    public static Header header;
+    public static CartPage cartPage;
 
 
     @BeforeClass
@@ -31,13 +37,22 @@ public class BaseTest {
 
     @BeforeMethod
     public void pageSetUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", Map.of(
+                "credentials_enable_service", false,
+                "profile.password_manager_enabled", false,
+                "profile.password_manager_leak_detection", false
+        ));
+        driver = new ChromeDriver(options);
+
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.navigate().to(URLs.STARTING_PAGE_URL);
 
         startingPage = new StartingPage();
         productsPage = new ProductsPage();
+        header = new Header();
+        cartPage = new CartPage();
     }
 
     @AfterMethod
